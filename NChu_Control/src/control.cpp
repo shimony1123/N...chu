e5ae8523201fca98c1567dcd296673bb575ca36c
kenkenmaru09 <kasaiken1005@gmail.com>
2023-05-26 17:39:36 +0900

    P_controlを修正

--- NChu_Control/src/control.cpp
+++ NChu_Control/src/control.cpp
@@ -1,13 +1,12 @@
 #include "ArduinoEigen.h"
-#include "ArduinoEigen.h"
 
-float P_control(Eigen::Vector3f euler,float channel1,float channel2){
+ Eigen::Vector2f P_control(Eigen::Vector3f euler,float channel1,float channel2){
     //引数eulerにはカルマンフィルタを通した後のオイラー角が入る。
     float duty_roll; //出力するPWMのduty比
     float duty_pitch; //出力するPWMのduty比
     
-    float g_roll = 0.01; //このP制御のゲインをチューニングする。
-    float g_pitch = 0.01; //このP制御のゲインをチューニングする。
+    float g_roll = 0.01; //rollのP制御のゲイン。要チューニング。
+    float g_pitch = 0.01; //pitchのP制御のゲイン。要チューニング。
 
     float channel_min = 368;
     float channel_max = 1680;
@@ -23,6 +22,9 @@ float P_control(Eigen::Vector3f euler,float channel1,float channel2){
     float diff_roll = euler(0) - roll_obj;
     float diff_pitch = euler(1) - pitch_obj;
 
-    duty_roll = g_roll*diff_roll;
-    duty_pitch = g_pitch*diff_pitch;
+    Eigen::Vector2f duty; //duty[0]がrollのduty比、duty[1]がpitchのduty比
+    duty[0] = g_roll*diff_roll;
+    duty[1] = g_pitch*diff_pitch;
+
+    return duty;
 }
